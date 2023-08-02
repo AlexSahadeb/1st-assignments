8bf015e757a3689eac309ca9dd175fdc7ad6ae9d
Sahadeb <AlexSahadeb>
2023-08-02 22:30:57 +0600

    first commit

---
+++ bin/main.dart
@@ -0,0 +1,23 @@
+void main() {
+  //Addition
+  int a = 7;
+  int b = 3;
+  print(a + b);
+   //Subtraction  
+  int c = 7;
+  int d = 3;
+  print(c - d);
+   //Multiplication
+  int e = 7;
+  int f = 3;
+  print(e * f);
+   //Division
+  int g = 7;
+  int h = 3;
+  print(g / h);
+  
+  //Modulus
+  int i = 7;
+  int j = 3;
+  print(i % j);
+}
